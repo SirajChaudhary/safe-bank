a5b50736f6345feb71fbcd87636ba39ca7bb4f4c
Siraj Chaudhary <mr.sirajchaudhary@yahoo.com>
2025-09-28 21:50:37 +0530

    supporting multiple users via InMemoryUserDetailsManager

--- safe-bank-backend-service/src/main/java/com/company/safebank/config/BasicSecurityConfig.java
+++ safe-bank-backend-service/src/main/java/com/company/safebank/config/BasicSecurityConfig.java
@@ -3,6 +3,10 @@ package com.company.safebank.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -12,11 +16,19 @@ public class BasicSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        /*http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());*/
-        /*http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());*/
-        http.authorizeHttpRequests((requests) -> requests.requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated().requestMatchers("/notices", "/contact", "/error").permitAll());
+        http.authorizeHttpRequests((requests) ->
+                requests.requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated().
+                requestMatchers("/notices", "/contact", "/error").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        //Here noop indicate we don't want to use any password encoder and keep password as plaintext
+        UserDetails user = User.withUsername("user").password("{noop}12345").authorities("read").build();
+        UserDetails admin = User.withUsername("admin").password("{noop}54321").authorities("admin").build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
